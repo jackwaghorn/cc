9a56eb4905e30986f271406392c8b1d8f1721188
jackwaghorn <47627707+jackwaghorn@users.noreply.github.com>
2021-02-10 21:01:09 +0000

    First commit

---
+++ canvas.js
@@ -0,0 +1,94 @@
+//RANDOM IMAGE
+let num;
+num = Math.floor(Math.random()* 16 + 1);
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("canvas").style.backgroundImage = "url('img/"+ num +".jpg')";
+
+    if(num == 1 ) {
+        document.getElementById("caption").innerHTML = 'CC Halloween Show. Photograph: Will Clark'; };
+    if(num == 2 ) {
+        document.getElementById("caption").innerHTML = 'Kespos Arts Gathering. Photograph: Danielle and Lexi Goodland'; };
+    if(num == 3 ) {
+        document.getElementById("caption").innerHTML = 'Kespos Arts Gathering. Photograph: Danielle and Lexi Goodland'; };
+    if(num == 4 ) {
+        document.getElementById("caption").innerHTML = 'Label Launch. Photograph: Jack Joseph'; };
+    if(num == 5 ) {
+        document.getElementById("caption").innerHTML = 'CC Halloween Show. Photograph: Will Clark'; };
+    if(num == 6 ) {
+        document.getElementById("caption").innerHTML = 'Kespos Opening Night. Photograph: Lexi Goodland'; };
+    if(num == 7 ) {
+        document.getElementById("caption").innerHTML = 'Kespos Arts Gathering. Photograph: Danielle and Lexi Goodland'; };
+    if(num == 8 ) {
+        document.getElementById("caption").innerHTML = 'Label Launch. Photograph: Jack Joseph'; };
+    if(num == 9 ) {
+        document.getElementById("caption").innerHTML = 'Label Launch. Photograph: Jack Joseph'; };
+    if(num == 10 ) {
+        document.getElementById("caption").innerHTML = 'Kespos Arts Gathering. Photograph: Danielle and Lexi Goodland'; };
+    if(num == 11 ) {
+        document.getElementById("caption").innerHTML = 'Jacobs Ladder. Photograph: Will Clark'; };
+    if(num == 12 ) {
+        document.getElementById("caption").innerHTML = 'CC Halloween Show. Photograph: Will Clark'; };
+    if(num == 13 ) {
+        document.getElementById("caption").innerHTML = 'Jacobs Ladder. Photograph: Will Clark'; };
+    if(num == 14 ) {
+        document.getElementById("caption").innerHTML = 'CC Halloween Show. Photograph: Will Clark'; };
+    if(num == 15 ) {
+        document.getElementById("caption").innerHTML = 'CC Halloween Show. Photograph: Will Clark'; };
+    if(num == 16 ) {
+        document.getElementById("caption").innerHTML = 'Jacobs Ladder. Photograph: Will Clark'; };
+    
+});
+
+//CAPTION
+
+
+//DRAW
+window.addEventListener("load", () => {
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext("2d");
+
+    //Resizing
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    //Variables
+    let painting = false;
+
+    function startPosition(e){
+        painting = true;
+        draw(e);
+    }
+    function finishedPosition(){
+        painting = false;
+        ctx.beginPath();
+    }
+
+    function draw(e){
+
+
+        
+        if (!painting) return;
+        ctx.lineWidth = 10;
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = 'white';
+
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY);
+    }
+
+    //EventListeners
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mouseup", finishedPosition);
+    canvas.addEventListener("mousemove", draw);
+});
+
+
+//Resize on stretch
+window.addEventListener('resize', () => {
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+});
+
